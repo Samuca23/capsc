02d3e5c7c8256938ab2b388f84e2cde3aeeafca3
Samuca23 <samuelchiodini@hotmail.com>
2024-01-13 09:36:25 -0300

    back end

---
+++ capsc/src/main/java/br/com/unidavi/capsc/CapscApplication.java
@@ -0,0 +1,13 @@
+package br.com.unidavi.capsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapscApplication.class, args);
+	}
+
+}
---
+++ capsc/src/test/java/br/com/unidavi/capsc/CapscApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unidavi.capsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
